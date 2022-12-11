18be63081d2a788963640f05ebab515c8d0806f8
Diamond Sword <95223065+diamond2sword@users.noreply.github.com>
2022-12-11 21:40:56 +0800

    Add files via upload

---
+++ project-raspberrypi/project-v1/printer.py
@@ -0,0 +1,15 @@
+
+
+
+
+def print_pic(pic):
+    pic = to_printable_image(pic)
+    print(f"\n{pic}")
+    pass
+
+
+def print_str(string):
+    print(string)
+
+
+from term_image.image import AutoImage as to_printable_image
