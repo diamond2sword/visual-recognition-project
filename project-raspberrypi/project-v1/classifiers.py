768b14fb181ec32faecdb44e63b0a5d599242a4f
Diamond Sword <95223065+diamond2sword@users.noreply.github.com>
2022-12-16 22:30:56 +0800

    Add files via upload

--- project-raspberrypi/project-v1/classifiers.py
+++ project-raspberrypi/project-v1/classifiers.py
@@ -1,11 +1,11 @@
 	
 class Classifier:
-	def __main(self)->int:
+	def main(self)->int:
 		self.__run()
 		return 0
 		
 	def __run(self):
-		self.__reset_variables()
+		self.__reset_run_variables()
 		self.__warm_up_camera()
 		self.__start_time_progressbar()
 		self.__reset_stop_time()
@@ -21,23 +21,25 @@ class Classifier:
 				continue
 			self.__realtime_classify()
 			self.__add_last_probabilities_to_sum()
-			self.__clear_time_progressbar()
-			self.__show_probabilities()
-			self.__update_time_progressbar()
+			self.__change_realtime_output()
+			self.__show_realtime_output()
 		self.__end_time_progressbar()
 		
-	def __show_probabilities(self):
-		self.__show_last_probabilities()
-		self.__show_sum()
+	def __show_realtime_output(self):
+		self.__print_with_respect_to_progressbar(self.realtimeOutput)
+		
+	def __change_realtime_output(self):
+		self.__change_realtime_output_to_last_probabilities()
+		self.__change_realtime_output_to_average_probabilities()
+		self.__change_realtime_output_to_labeled_probabilities()
 		
 	def __add_last_probabilities_to_sum(self):
 		self.sum = add_array(self.sum, self.lastProbabilities)
 		self.addCount += 1
-		return self.sum
 		
 	def __realtime_classify(self):
 		scores = self.model.run(None, {self.modelInputName: self.inputPic})
-		self.lastProbabilities = exp(scores)	
+		self.lastProbabilities = exp(scores)
 		
 	def __check_classify_key(self):
 		if waitKey(1) == self.unicodeOfClassifyKey:
@@ -67,14 +69,27 @@ class Classifier:
 	def __check_for_stop(self):
 		return self.__check_stop_time() or self.__check_stop_key()
 		
-	def __show_sum(self):
-		averageProbabilities = divide_array(self.sum, self.addCount)
-		self.__print(averageProbabilities)
 		
-	def __show_last_probabilities(self):
-		self.__print(self.lastProbabilities)
+	def __change_realtime_output_to_labeled_probabilities(self):
+		self.labeledRealtimeOutput = dict(zip(self.classLabels, self.realtimeOutput[0][0]))
+		self.realtimeOutput = self.labeledRealtimeOutput
+		self.__change_realtime_output_to_sorted_probabilities()
 		
-	def __print(self, string):
+	def __change_realtime_output_to_sorted_probabilities(self):
+		self.realtimeOutput = sorted(
+			self.labeledRealtimeOutput.items(), 
+			key=lambda item: item[1], 
+			reverse=True
+		)
+		
+	def __change_realtime_output_to_average_probabilities(self):
+		self.averageProbabilities = divide_array(self.sum, self.addCount)
+		self.realtimeOutput = self.averageProbabilities
+		
+	def __change_realtime_output_to_last_probabilities(self):
+		self.realtimeOutput = self.lastProbabilities
+	
+	def __print_with_respect_to_progressbar(self, string):
 		self.__clear_time_progressbar()
 		print(string)
 		self.__update_time_progressbar()
@@ -120,13 +135,18 @@ class Classifier:
 		mustWaitClassifyKey=False,
 		pauseKey=None,
 		hasTimeProgressbar=False,
-		isSummed=True,
+		isSummed=False,
 		isRealtime=True,
+		isRealtimeOutputLabeled=False,
+		isRealtimeOutputSorted=True,
 		):
 		self.stopTime = stopTime
 		self.stopKey = stopKey
 		self.__def_stop_time()
 		self.__def_stop_key()
+		
+		self.hasTimeProgressbar = hasTimeProgressbar
+		self.__def_time_progressbar()
 		
 		self.pauseKey = pauseKey
 		self.__def_check_pause_key()
@@ -137,6 +157,8 @@ class Classifier:
 		self.model = get_onnx_model()
 		self.modelInputName = get_input_name_of(self.model)
 		
+		self.classLabels = get_class_labels()
+		
 		self.hasPreview = hasPreview
 		self.picWindowName = picWindowName
 		self.__def_show_preview()
@@ -145,21 +167,22 @@ class Classifier:
 		self.mustWaitClassifyKey = mustWaitClassifyKey
 		self.__def_check_classify_key()
 		
-		self.hasTimeProgressbar = hasTimeProgressbar
-		self.__def_time_progressbar()
-		
-		self.isSummed = isSummed
 		self.isRealtime = isRealtime
+		self.isSummed = isSummed
+		self.isRealtimeOutputLabeled = isRealtimeOutputLabeled
+		self.isRealtimeOutputSorted = isRealtimeOutputSorted
 		self.__def_get_pictures()
 		self.__def_save_input_pic()
 		self.__def_realtime_classify()
 		self.__def_add_last_probabilities_to_sum()
-		self.__def_show_probabilities()
+		self.__def_change_realtime_output()
+		self.__def_show_realtime_output()
 		self.__def_run()
-		
-		self.__reset_variables()
+
+		self.__reset_run_variables()
 	
-	def __reset_variables(self):
+	def __reset_run_variables(self):
+		self.timeProgressBar = None
 		self.picArray = None
 		self.pilPic = None
 		self.centeredPic = None
@@ -168,25 +191,29 @@ class Classifier:
 		self.sum = 0
 		self.addCount = 0
 		self.lastProbabilities = None
-		self.timeProgressBar = None
-		
-	def __def_time_progressbar(self):
-		if not self.hasTimeProgressbar or self.stopTime is None:
-			self.__start_time_progressbar = self.__do_nothing
-			self.__clear_time_progressbar = self.__do_nothing
-			self.__update_time_progressbar = self.__do_nothing
-			self.__end_time_progressbar = self.__do_nothing
+		self.averageProbabilities = None
+		self.labeledRealtimeOutput = None
+		self.realtimeOutput = None
 		
 	def __def_run(self):
 		if not self.isRealtime and not self.isSummed:
 			self.__run = self.__do_nothing
 	
-	def __def_show_probabilities(self):
+	def __def_show_realtime_output(self):	
 		if not self.isRealtime:
-			self.__show_probabilities = self.__do_nothing
+			self.__show_realtime_output = self.__do_nothing
+	
+	def __def_change_realtime_output(self):
+		if not self.isRealtime:
+			self.__change_realtime_output = self.__do_nothing
+			self.__change_realtime_output_to_last_probabilities = self.__do_nothing
 		if not self.isSummed:
-			self.__show_sum = self.__do_nothing
-		
+			self.__change_realtime_output_to_average_probabilities = self.__do_nothing
+		if not self.isRealtimeOutputLabeled:
+			self.__change_realtime_output_to_labeled_probabilities = self.__do_nothing
+		if not self.isRealtimeOutputSorted:
+			self.__change_realtime_output_to_sorted_probabilities = self.__do_nothing
+			
 	def __def_add_last_probabilities_to_sum(self):
 		if not self.isRealtime or not self.isSummed:
 			self.__add_last_probabilities_to_sum = self.__do_nothing
@@ -230,6 +257,13 @@ class Classifier:
 			self.unicodeOfPauseKey = ord(self.pauseKey)
 		if self.pauseKey is None:
 			self.__check_pause_key = self.__do_nothing
+		
+	def __def_time_progressbar(self):
+		if not self.hasTimeProgressbar or self.stopTime is None:
+			self.__start_time_progressbar = self.__do_nothing
+			self.__clear_time_progressbar = self.__do_nothing
+			self.__update_time_progressbar = self.__do_nothing
+			self.__end_time_progressbar = self.__do_nothing
 			
 	def __def_stop_time(self):
 		if self.stopTime is None:
@@ -254,12 +288,13 @@ from config import *
 from class_dict_manager import *
 from onnx import *
 from dataset import *
+from camera import *
 from cv2 import imshow as request_to_display, waitKey
 from torchvision.transforms import Resize, CenterCrop
 from torch.nn import Sequential
-from camera import *
 from numpy import add as add_array, divide as divide_array
 from tqdm import tqdm as Progressbar
 import sys
 if __name__ == "__main__":
-	c = Classifier()
+	c = Classifier(hasPreview=True, hasTimeProgressbar=False, isSummed=False)
+	c.main()
