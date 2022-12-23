385e755d468c612186c5916c792512d0a4f7c017
diamond2sword <diamond2sword@gmail.com>
2022-12-24 03:13:13 +0800

    update project

--- copy_project.sh
+++ copy_project.sh
@@ -2,11 +2,13 @@
 
 temp_path="/root/Desktop/project"
 repo_path="/root/visual-recognition-project/project-raspberrypi/project-v1"
+git_path="/root/visual-recognition-project/git.sh"
 
 x-terminal-emulator -e '{
 	#!/bin/bash
 	cp -vf $(ls -d  '"$temp_path"'/* | sed -n "/^.*[py|sh]$/p" | tr "\n" " ") '"$repo_path"'
 	diff '"$temp_path"' '"$repo_path"'
+	'"$git_path"' GIT_PUSH
 	read 
 }'
 
